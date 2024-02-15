7eb4080984e8de633738fe1df350a174e9ad69e3
CasiopeaCloudMintJams <157869776+CasiopeaCloudMintJams@users.noreply.github.com>
2024-02-15 02:03:59 -0600

    Update 4-Pong.py

--- 4-Pong.py
+++ 4-Pong.py
@@ -6,7 +6,7 @@
 
 ##import simpleguitk as simplegui
 
-import simpleguitk as simplegui
+import PySimpleGUI as simplegui
 import random
 
 # ----------------------------------------------------------------
